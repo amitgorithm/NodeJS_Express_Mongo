9df3bfeec8cb3cd5c8e9b05370a07b646977ed9e
Amit Jape <japeamit@gmail.com>
2025-07-02 15:46:35 +0530

    Hands on with Express Framework

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,60 @@
 const http = require("http");
 const fs = require('fs');
-const url = require('url');
+// const url = require('url');
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    return res.end("Hello From Home Page");
+})
+
+app.get("/about", (req,res) => {
+    return res.end("Hello from About Page" + "Your name is : " + req.query.name);
+})
+
+app.get("/signup", (req,res)=>{
+    return res.end("This is the Signup Page" + ' hey ' + req.query.name + ' you are ' + req.query.age);
+})
+
+app.listen(8000, () => console.log("Server Started via Express!"));
+
+// const myServer = http.createServer(app);
+
+
+function myHandler(req,res) {
+    if(req.url === '/favicon.ico'){return res.end()};
+    const log = `${Date.now()}: ${req.method} : ${req.url} New Req Recieved\n`;
+    const myUrl = url.parse(req.url, true);
+    // console.log(myUrl);
+
+      fs.appendFile('log.txt', log, (err,data) => {
+          switch(myUrl.pathname){
+              case '/': 
+              if(req.method === 'GET') res.end('HomePage')
+                break;
+              case '/about':
+                const username = myUrl.query.myname;
+                res.end(`Hi: ${username}`);
+                break;
+              case '/search':
+                const search = myUrl.query.search_query;
+                res.end("Your search is for : " + search);
+              case '/signup':
+                if(req.method==='GET') {
+                  res.end("This is a signup form");
+                } else if(req.method==='POST'){
+                  // DB Query
+                    res.end("Success");
+                }
+              default:
+                  res.end("404 Not Found");
+
+          }
+          
+     } );
+      
+    }
 
 // we are creating alog for the users who hit the server/port
 // we created a variable 'log'that stores the date.now 
@@ -8,34 +62,7 @@ const url = require('url');
 // then we use callback. in fs.appendFile (err,data) =>
   // and if all goes well, we end the response with 
     // res.end("hello from server")
-const myServer = http.createServer((req,res) => {
-    if(req.url === '/favicon.ico'){return res.end()};
-    const log = `${Date.now()}: ${req.url} New Req Recieved\n`;
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
 
-     fs.appendFile('log.txt', log, (err,data) => {
-          switch(myUrl.pathname){
-              case '/': 
-                res.end ("HomePage");
-                break;
-              case '/about':
-                const username = myUrl.query.myname;
-                res.end(`Hi: ${username}`);
-
-                break;
-              case '/search':
-                const search = myUrl.query.search_query;
-                res.end("Your search is for : " + search);
-              default:
-                  res.end("404 Not Found");
-
-          }
-          
-     } );
-  
-
-});
 
 // to create a server
 // req and res are two arguments of the callback function
@@ -55,4 +82,4 @@ const myServer = http.createServer((req,res) => {
 // to run a server we need a port and port number
 // on this port, we will make the 
 // make the server listen for the incoming request.
-myServer.listen(8000, () => console.log("Server Started") );
+// myServer.listen(8000, () => console.log("Server Started") );
