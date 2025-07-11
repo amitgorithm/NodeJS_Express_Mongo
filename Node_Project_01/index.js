927b24a0fa93d45cbcf3dd5ba7e6d2cbb5ca2b8a
Amit Jape <japeamit@gmail.com>
2025-07-11 06:23:46 +0530

    HTTP Status Code and MongoDB Connection

--- Node_Project_01/index.js
+++ Node_Project_01/index.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const fs = require('fs');
+const mongoose = require('mongoose');
+
 const users = require("./MOCK_DATA.json")
 
 const app = express();
 const PORT = 8000;
+
+// Connection to mongo
+mongoose.connect('mongodb://127.0.0.1:27017/youtube-app-1')
+.then(()=> console.log("MongoDB Connected"))
+.catch(err => console.log("Mongo Error", err));
+
+// Schema 
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName:  {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  jobTitle: {
+    type: String,
+  },
+  gender:{
+    type:String,
+  }
+})
+
+const User = mongoose.model('user', userSchema);
 
 //ROUTES 
 app.get("/users", (req,res) => {
@@ -40,7 +71,8 @@ app
     .route("/api/users/:id")
       .get((req,res)=> {
           const id = Number(req.params.id);
-          const user = users.find((user) => user.id === id)
+          const user = users.find((user) => user.id === id);
+          if(!user) return res.status(404).json({error : "User does not exist"});
           return res.json(user);
         })
       .patch((req,res) => {
@@ -89,12 +121,29 @@ app.get("/api/users", (req,res) => {
     return res.json(users);
 });      
 
-app.post("/api/users", (req,res) => {
+app.post("/api/users", async (req,res) => {
       const body = req.body;
-      users.push({...body, id:  users.length + 1});
-      fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
-           res.json({status: "success", id: users.length});
-      });
+      if(!body || 
+        !body.first_name || 
+        !body.last_name ||
+        !body.email || 
+        !body.gender || 
+        !body.job_title) 
+        { 
+        return res.status(400).json({msg: 'All fields are required'})
+        } ;
+      
+    const result = await User.create({
+      firstName: body.first_name,
+      lastName: body.last_name,
+      email: body.email,
+      gender: body.gender,
+      jobTitle: body.job_title
+    });
+
+    console.log('result');
+
+    return res.status(202).json({msg: "success"});
      
 });
 
